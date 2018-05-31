689da2e7cc1ad3368ddcd735dee870a06a5f8100
20123161 <maxime.tasset@student.ulg.ac.be>
2018-05-31 15:34:01 +0200

    image prediction updtate (work in progress)

---
+++ Cytomine-python-client-fork/addSofware_prediction.py
@@ -0,0 +1,133 @@
+# -*- coding: utf-8 -*-
+#
+# * Copyright (c) 2009-2015. Authors: see NOTICE file.
+# *
+# * Licensed under the Apache License, Version 2.0 (the "License");
+# * you may not use this file except in compliance with the License.
+# * You may obtain a copy of the License at
+# *
+# *      http://www.apache.org/licenses/LICENSE-2.0
+# *
+# * Unless required by applicable law or agreed to in writing, software
+# * distributed under the License is distributed on an "AS IS" BASIS,
+# * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# * See the License for the specific language governing permissions and
+# * limitations under the License.
+# */
+
+
+__author__          = "Maxime Tasset <maxime.tasset@student.ulg.ac.be>"
+__copyright__       = "Copyright 2010-2015 University of Liège, Belgium, http://www.cytomine.be/"
+
+from cytomine import Cytomine
+from cytomine.models import Software, SoftwareParameter,SoftwareProject
+
+cytomine_host="demo.cytomine.be"
+cytomine_public_key="XXX"
+cytomine_private_key="XXX"
+id_project= XXX
+save_path = "/tmp"
+
+def main(argv):
+    with Cytomine.connect_from_cli(argv):
+        software = Software(name="Feature_Selection",
+                            service_name="pyxitSuggestedTermJobService",
+                            result_name="ValidateAnnotation").save()
+
+#        SoftwareParameter("cytomine_id_software", type="Number", id_software=software.id,
+#                          index=100, set_by_server=True, required=True).save()
+#        SoftwareParameter("cytomine_id_project", type="Number", id_software=software.id,
+#                          index=100, set_by_server=True, required=True).save()
+
+        # filtering annotations
+        SoftwareParameter("cytomine_imagegroup", type="Domain", id_software=software.id, index=800, default_value='',
+                          uri="/api/project/$currentProject$/imagegroup.json",uri_sort_attribut="id",uri_print_attribut="id").save()
+        SoftwareParameter("cytomine_tile_size", type="Number", id_software=software.id, index=900, default_value=30).save()
+
+        SoftwareParameter("startx", type="Number", id_software=software.id, index=1000, default_value=0).save()
+        SoftwareParameter("starty", type="Number", id_software=software.id, index=1100, default_value=1000).save()
+        SoftwareParameter("endx", type="Number", id_software=software.id, index=1200, default_value=0).save()
+        SoftwareParameter("endy", type="Number", id_software=software.id, index=1300, default_value=1000).save()
+
+        SoftwareParameter("cytomine_predict_term", type="ListDomain", id_software=software.id, index=1400, default_value='',
+                          uri="/api/project/$currentProject$/term.json",uri_sort_attribut="name",uri_print_attribut="name").save()
+
+        # running parameters
+        SoftwareParameter("n_jobs", type="Number", id_software=software.id, default_value=1, index=1500).save()
+        SoftwareParameter("n_jobs_reader", type="Number", id_software=software.id, default_value=10, index=1600).save()
+
+        SoftwareParameter("model_path", type="String", id_software=software.id, default_value="/tmp", index=1700).save()
+        SoftwareParameter("model_name", type="String", id_software=software.id, default_value="model.pickle", index=1800).save()
+        SoftwareParameter("model_nb_jobs", type="Number", id_software=software.id, default_value=4, index=1900).save()
+
+        SoftwareParameter("cytomine_overlap", type="Number", id_software=software.id, default_value=10, index=2000).save()
+
+
+
+        print(software.id)
+        SoftwareProject(software.id,id_project).save()
+        return software
+
+
+if __name__ == "__main__":
+    import sys
+    import logging
+    if len(sys.argv[1:]):
+      software = main(sys.argv[1:])
+    else:
+      argv = ['--cytomine_host',cytomine_host,
+              "--cytomine_public_key",cytomine_public_key,
+              "--cytomine_private_key",cytomine_private_key]
+      software = main(argv)
+      from feature_selection import main as main_feature
+      argv.extend(["--cytomine_id_project",str(id_project),
+                   "--cytomine_id_software",str(software.id),
+                   "--cytomine_predict_term",'', #list of the terms names used for the feature selection format 'name1,name2,name3' (note: if '', all terms will be used)
+                   "--cytomine_positive_predict_term",'', #list of the terms names that will be merge format 'name1,name2,name3' (note: if '', no merge)
+                   "--cytomine_users_annotation",'', #the annotations which have as user, one in the list will be used, not the others (note: if '' all annotation will be used)
+                   "--cytomine_imagegroup",'', #list of the project imagegroup's id that will be used (note: if '' all imagegroup will be used)
+                   "--n_jobs",str(4),
+                   "--forest_max_features","auto",
+                   "--forest_n_estimators",str(10),
+                   "--forest_min_samples_split",str(2),
+                   "--save_path",save_path]) # complete path where the 'results.csv' will be saved
+      try:
+        job = main_feature(argv)
+      finally:
+
+        with Cytomine(host="demo.cytomine.be", public_key=cytomine_public_key, private_key=cytomine_private_key,
+                  verbose=logging.INFO) as cytomine:
+                      software.delete()
+
+
+
+
+#import cytomine
+#import sys
+#
+##Connect to cytomine, edit connection values
+#cytomine_host="demo.cytomine.be"
+#cytomine_public_key="f1f8cacc-b71a-4bc2-a6cd-e6bb40fd19b5"
+#cytomine_private_key="9e94aa70-4e7c-4152-8067-0feeb58d42eb"
+#id_project=28146931
+#
+##Connection to Cytomine Core
+#conn = cytomine.Cytomine(cytomine_host, cytomine_public_key, cytomine_private_key, base_path = '/api/', working_path = '/tmp/', verbose= False)
+#
+##define software parameter template
+#
+#software = conn.add_software("Feature_Selection", "pyxitSuggestedTermJobService","ValidateAnnotation")
+#
+#conn.add_software_parameter("cytomine_predict_term",software.id,"ListDomain",None,False,10,False,"/api/project/$currentProject$/term.json","name","name")
+#conn.add_software_parameter("cytomine_positive_predict_term",software.id,"ListDomain",None,True,10,False,"/api/project/$currentProject$/term.json","name","name")
+#conn.add_software_parameter("cytomine_users_annotation",software.id,"ListDomain",None,False,20,False,"/api/project/$currentProject$/user.json","username","username")
+#conn.add_software_parameter("cytomine_imagegroup",software.id,"ListDomain",None,False,30,False,"/api/project/$currentProject$/imagegroup.json","id","id")
+#
+#conn.add_software_parameter("forest_n_estimators", software.id, "Number", 10, True, 1100, False)
+#conn.add_software_parameter("forest_min_samples_split", software.id, "Number", 2, True, 1300, False)
+#conn.add_software_parameter("forest_max_features", software.id, "String", 'auto', True, 1400, False)
+#
+#conn.add_software_parameter("pyxit_save_to", software.id, "String", "/tmp", False, 1500, False)
+#
+##add software to a given project
+#addSoftwareProject = conn.add_software_project(id_project,software.id)
--- Cytomine-python-client-fork/image_prediction_wholeslide_spectral.py
+++ Cytomine-python-client-fork/image_prediction_wholeslide_spectral.py
@@ -29,17 +29,11 @@ __copyright__       = "Copyright 2010-2016 University of Liège, Belgium, http:/
 #in the whole image, apply post-processing based on min/max are, apply a final classifier on geometries,
 #and finally output statistics (counts/area).
 
-try:
-    import Image, ImageStat
-except:
-    from PIL import Image, ImageStat
-
 import sys
 
 import pickle
 
-from progressbar import *
-import os, optparse
+import os
 from time import localtime, strftime
 
 
@@ -47,48 +41,16 @@ from shapely.geometry import Polygon,MultiPolygon
 import numpy as np
 
 import cytomine
-from cytomine.utilities.reader import Bounds, CytomineSpectralReader
+from cytomine.utilities import Bounds, CytomineSpectralReader
 from cytomine.spectral.extractor import coordonatesToPolygons,polygonToAnnotation
 
-from cytomine.models.user import User,CurrentUser
-from cytomine.models.software import Job
+from cytomine.models import User,CurrentUser,Job,Annotation
 
 from multiprocessing import Pool
 
 from shapely import wkt
 
-#Parameter values are now set through command-line
-parameters = {
-'cytomine_host' : None,
-'cytomine_public_key' : None,
-'cytomine_private_key' : None,
-'cytomine_base_path' : None,
-'cytomine_working_path' : '/home/maree/tmp/cytomine/',
-'cytomine_id_software' : 0,
-'cytomine_id_project' : 0,
-'cytomine_id_image' : None,
-'cytomine_zoom_level' : 0,
-'cytomine_tile_size' : 512,
-'cytomine_tile_min_stddev' : 0,
-'cytomine_tile_max_mean' : 255,
-'cytomine_predict_term' : 0,
-'cytomine_union' : False,
-'cytomine_postproc' : False,
-'cytomine_count' : False,
-'cytomine_min_size' : 0,
-'cytomine_max_size' : 1000000000,
-'cytomine_roi_term': None,
-'cytomine_reviewed_roi': None,
-'pyxit_target_width' : 24,
-'pyxit_target_height' : 24,
-'pyxit_predict_step' : 1,
-'pyxit_save_to' : '/home/maree/tmp/cytomine/models/test.pkl',
-'pyxit_colorspace' : 2,
-'pyxit_nb_jobs' : 10,
-'nb_jobs' : 20,
-'publish_annotations': True ,
-
-}
+from cytomine import CytomineJob
 
 
 
@@ -106,7 +68,12 @@ def str2bool(v):
 def main(argv):
     current_path = os.getcwd() +'/'+ os.path.dirname(__file__)
     # Define command line options
+
+
     print("Main function")
+    with CytomineJob.from_cli(argv,verbose=logging.WARNING) as cj:
+      id_project = cj.project.id
+
     p = optparse.OptionParser(description='Cytomine Segmentation prediction',
                               prog='Cytomine segmentation prediction',
                               version='0.1')
@@ -129,154 +96,103 @@ def main(argv):
     p.add_option('--endy', type='int', dest='cytomine_endy', help="end y position")
     p.add_option('--cytomine_predict_term', type='int', dest='cytomine_predict_term', help="term id of predicted term (binary mode)")
 
-    p.add_option('--pyxit_target_width', type='int', dest='pyxit_target_width', help="pyxit subwindows width")
-    p.add_option('--pyxit_target_height', type='int', dest='pyxit_target_height', help="pyxit subwindows height")
     p.add_option('--pyxit_save_to', type='string', dest='pyxit_save_to', help="pyxit segmentation model file") #future: get it from server db
 
     p.add_option('--cytomine_overlap', type='int', dest='cytomine_overlap', help="reader overlap")
 
-    p.add_option('--pyxit_post_classification', type="string", default="0", dest="pyxit_post_classification", help="pyxit post classification of candidate annotations")
-
-    p.add_option('--pyxit_nb_jobs', type='int', dest='pyxit_nb_jobs', help="pyxit number of jobs for trees") #future: get it from server db
-
-    p.add_option('--verbose', type='string', default="0", dest="verbose", help="Turn on (1) or off (0) verbose mode")
+    p.add_option('--model_nb_jobs', type='int', dest='pyxit_nb_jobs', help="pyxit number of jobs for trees") #future: get it from server db
 
 
     options, arguments = p.parse_args( args = argv)
+    if False:
+        SoftwareParameter("cytomine_imagegroup", type="Domain", id_software=software.id, index=800, default_value='',
+                          uri="/api/project/$currentProject$/imagegroup.json",uri_sort_attribut="id",uri_print_attribut="id").save()
+        SoftwareParameter("cytomine_tile_size", type="Number", id_software=software.id, index=900, default_value=30).save()
 
-    parameters['cytomine_host'] = options.cytomine_host
-    parameters['cytomine_public_key'] = options.cytomine_public_key
-    parameters['cytomine_private_key'] = options.cytomine_private_key
+        SoftwareParameter("startx", type="Number", id_software=software.id, index=1000, default_value=0).save()
+        SoftwareParameter("starty", type="Number", id_software=software.id, index=1100, default_value=1000).save()
+        SoftwareParameter("endx", type="Number", id_software=software.id, index=1200, default_value=0).save()
+        SoftwareParameter("endy", type="Number", id_software=software.id, index=1300, default_value=1000).save()
 
-    parameters['cytomine_base_path'] = options.cytomine_base_path
-    parameters['cytomine_id_project'] = options.cytomine_id_project
-    parameters['cytomine_id_software'] = options.cytomine_id_software
-    parameters['cytomine_predict_term'] = options.cytomine_predict_term
-    parameters['model_id_job'] = 0
+        SoftwareParameter("cytomine_predict_term", type="ListDomain", id_software=software.id, index=1400, default_value='',
+                          uri="/api/project/$currentProject$/term.json",uri_sort_attribut="name",uri_print_attribut="name").save()
 
-    parameters['pyxit_save_to'] = options.pyxit_save_to
+        # running parameters
+        SoftwareParameter("n_jobs", type="Number", id_software=software.id, default_value=1, index=1000).save()
+        SoftwareParameter("n_jobs_reader", type="Number", id_software=software.id, default_value=10, index=1000).save()
 
-    parameters['pyxit_nb_jobs'] = options.pyxit_nb_jobs
-    parameters['cytomine_nb_jobs'] = options.pyxit_nb_jobs
+        SoftwareParameter("model_path", type="String", id_software=software.id, default_value="/tmp", index=1600).save()
+        SoftwareParameter("model_name", type="String", id_software=software.id, default_value="model.pickle", index=1600).save()
+        SoftwareParameter("model_nb_jobs", type="Number", id_software=software.id, default_value=4, index=1600).save()
 
-    parameters['cytomine_id_imagegroup'] = options.cytomine_id_imagegroup
-
-    parameters['cytomine_tile_size'] = options.cytomine_tile_size
-    parameters['cytomine_overlap'] = options.cytomine_overlap
-
-    parameters['cytomine_startx'] = options.cytomine_startx
-    parameters['cytomine_starty'] = options.cytomine_starty
-    parameters['cytomine_endx'] = options.cytomine_endx
-    parameters['cytomine_endy'] = options.cytomine_endy
-    parameters['nb_jobs'] = options.nb_jobs
+        SoftwareParameter("cytomine_overlap", type="Number", id_software=software.id, default_value=10, index=1600).save()
 
 
-    print(parameters)
-
-
-    # Check for errors in the options
-    if options.verbose:
-        print("[pyxit.main] Options = ", options)
 
     #Initialization
     print("TIME : %s" %strftime("%Y-%m-%d %H:%M:%S", localtime()))
     print("Loading prediction model (local)")
-    fp = open(parameters["pyxit_save_to"], "r")
-    print(fp)
+    with open(os.path.join(cj.parameters.model_path,cj.parameters.model_name), "rb") as fb:
+        classifier = pickle.load(fp)
+        if hasattr(classifier,"set_N_Jobs"):
+            classifier.set_N_Jobs(cj.parameters.model_nb_jobs)
+        elif hasattr(classifier,"n_jobs"):
+            classifier.n_jobs = cj.parameters.model_nb_jobs
 
-    pyxit = pickle.load(fp)
-    pyxit.n_jobs = parameters['nb_jobs'] #multithread subwindows extraction in pyxit
-    pyxit.base_estimator.n_jobs= parameters['pyxit_nb_jobs']  #multithread tree propagation
+    n_jobs = cj.parameters.n_jobs
+    n_jobs_reader = cj.parameters.n_jobs_reader
+    predict_term = cj.parameters.cytomine_predict_term
+
     #Reading parameters
-    id_imagegroup= parameters['cytomine_id_imagegroup'] #int(sys.argv[1])
+
+    id_imagegroup= cj.parameters.cytomine_imagegroup #int(sys.argv[1])
 
 
-    #Create local directory to dump tiles
-    local_dir = "%s/slides/project-%d/tiles/" % (parameters['cytomine_working_path'], parameters["cytomine_id_project"])
-    if not os.path.exists(local_dir):
-        print("Creating tile directory: %s" %local_dir)
-        os.makedirs(local_dir)
+    tile_size = cj.parameters.cytomine_tile_size
+    overlap = cj.parameters.cytomine_overlap
+
+    cj.job.update(statusComment = "Initialization finished...")
 
 
 
-    print("TIME : %s" %strftime("%Y-%m-%d %H:%M:%S", localtime()))
-    print("Parameters: %s" %parameters)
-
-
-    #Cytomine connection
-    print("Connection to Cytomine server")
-    conn = cytomine.Cytomine(parameters["cytomine_host"],
-                             parameters["cytomine_public_key"],
-                             parameters["cytomine_private_key"],
-                             verbose= True)
-
-
-    print("Create Job and UserJob...")
-    id_software = parameters['cytomine_id_software']
-    #Create a new userjob if connected as human user
-    current_user = CurrentUser().fetch()
-    if current_user.algo==False:
-        print("adduserJob...")
-
-        job = Job( parameters['cytomine_id_project'], parameters['cytomine_id_software']).save()
-        user_job = User().fetch(job.userJob)
-        print("set_credentials...")
-        conn.set_credentials(str(user_job.publicKey), str(user_job.privateKey))
-        print("done")
-    else:
-        user_job = current_user
-        print("Already running as userjob")
-
-    job = Job().fetch(user_job.job)
-
-    job.statusComment = "Publish software parameters values"
-    job.update()
-
-
-#    job = conn.update_job_status(job, status = job.RUNNING, progress = 0, status_comment = "Loading data...")
-    job.statusComment = "Initialization of the reader"
-    job.status = job.RUNNING
-    job.progress = 0
-    job.update()
-
+    cj.job.update(statusComment = "Initialization of the reader", progress = 1)
 
 
     reader = CytomineSpectralReader(id_imagegroup,
                                     bounds = None,
-                                    tile_size = Bounds(0,0,parameters['cytomine_tile_size'],parameters['cytomine_tile_size']),
-                                    overlap=parameters['cytomine_overlap'],num_thread=parameters['cytomine_nb_jobs'])
+                                    tile_size = Bounds(0,0,tile_size,tile_size),
+                                    overlap=overlap,num_thread=n_jobs_reader)
 
-    x,y = reader.reverseHeight((parameters['cytomine_startx'],parameters['cytomine_endy']))
+    startx = cj.parameters.startx
+    starty = cj.parameters.starty
+    endx = cj.parameters.endx
+    endy = cj.parameters.endy
 
-    width = abs(parameters['cytomine_startx']-parameters['cytomine_endx'])
-    height = abs(parameters['cytomine_starty']-parameters['cytomine_endy'])
+    x,y = reader.reverseHeight((startx,endy))
+
+    width = abs(startx-endx)
+    height = abs(starty-endy)
+
     reader.setBounds(Bounds(x,y,width,height))
 
-    job.statusComment = "Start fetching data"
-    job.status = job.RUNNING
-    job.progress = 5
-    job.update()
+    cj.job.update(statusComment = "Start fetching data", progress = 5)
 
     results = MultiPolygon()
 
-    job.statusComment = "Initial read"
-    job.status = job.RUNNING
-    job.progress = 6
-    job.update()
+    cj.job.update(statusComment = "Initial read", progress = 6)
+
     for i in range(5):
       reader.read(async=True)
       if not reader.next():
         break
 
-    job.statusComment = "Start fetching data"
-    job.status = job.RUNNING
-    job.progress = 7
-    job.update()
+
+    cj.job.update(statusComment = "Start fetching data", progress = 7)
 
     stop = False
 
     iterate = 5
-    pool = Pool(parameters['nb_jobs'])
+    pool = Pool(n_jobs)
 
     while not stop:
       for i in range(iterate):
@@ -304,40 +220,30 @@ def main(argv):
       predictions = pyxit.predict(fetch)
 
       #get the coordonate of the pxl that correspond to the request
-      coord = [reader.reverseHeight(coord[index[0]]) for index in np.argwhere(predictions==parameters['cytomine_predict_term'])]
+      coord = [reader.reverseHeight(coord[index[0]]) for index in np.argwhere(predictions==predict_term)]
       if len(coord):
-        results = results.union(coordonatesToPolygons(coord,nb_job=parameters['nb_jobs'],pool,False))
+        results = results.union(coordonatesToPolygons(coord,nb_job=n_jobs,pool,False))
 
     reader.first_id
-    from cytomine.models.annotation import Annotation
+
     Annotation()
-    results = results.buffer(-.5).simplify(1,False)
+    results = results.buffer(-0.5).simplify(1,False)
 
-    job.statusComment = "Converting ROI to polygon"
-    job.status = job.RUNNING
-    job.progress = 90
-    job.update()
-
+    cj.job.update(statusComment = "Converting ROI to polygon", progress = 70)
 
     amp = pool.map(polygonToAnnotation,[p for p in resuls])
 
-    job.statusComment = "Uploading of the annotations on the first image of the imagegroup"
-    job.status = job.RUNNING
-    job.progress = 95
-    job.update()
+    cj.job.update(statusComment = "Uploading of the annotations on the first image of the imagegroup", progress = 95)
 
     for p in amp:
       Annotation(p,reader.first_id,
-                 id_terms=parameters['cytomine_predict_term'],
-                 id_project=parameters['cytomine_id_project']).save()
+                 id_terms=predict_term,
+                 id_project=id_project).save()
 
-    job.statusComment = "Finish Job.."
-    job.status = job.TERMINATED
-    job.progress = 100
-    job.update()
+
+    cj.job.update(statusComment = "Finish Job..", progress = 100)
 
     pool.close()
-    sys.exit()
 
 
 if __name__ == "__main__":
