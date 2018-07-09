9c7c2208a388eace22a3d8fc957818528d311dad
20123161 <maxime.tasset@student.ulg.ac.be>
2018-07-09 11:37:51 +0200

    add validation test

--- Cytomine-python-client-fork/test.py
+++ Cytomine-python-client-fork/test.py
@@ -75,13 +75,24 @@ sum_val = chi2_val + f_c_val + imp_val
 sumv = [i for val,i in sorted([(val,i) for i,val in enumerate(sum_val)])]
 del imp_val,f_c_val,chi2_val
 
-#Test and train sets
-test_SamplePCA_X,test_SamplePCA_Y = X[val[:int(0.2*len(val))]],ext.Y[val[:int(0.2*len(val))]]
-test_SampleX,test_SampleY = ext.X[val[:int(0.2*len(val))]],ext.Y[val[:int(0.2*len(val))]]
 
-train_SamplePCA_X,train_SamplePCA_Y = X[val[int(0.2*len(val)):]],ext.Y[val[int(0.2*len(val)):]]
-train_SampleX,train_SampleY = ext.X[val[int(0.2*len(val)):]],ext.Y[val[int(0.2*len(val)):]]
+test = .2
+validation = .1
+train = test + validation
 
+#Tes, train and validation sets
+test_SamplePCA_X,test_SamplePCA_Y = X[val[:int(test*len(val))]],ext.Y[val[:int(test*len(val))]]
+test_SampleX,test_SampleY = ext.X[val[:int(test*len(val))]],ext.Y[val[:int(test*len(val))]]
+
+train_SamplePCA_X,train_SamplePCA_Y = X[val[int(train*len(val)):]],ext.Y[val[int(train*len(val)):]]
+train_SampleX,train_SampleY = ext.X[val[int(train*len(val)):]],ext.Y[val[int(train*len(val)):]]
+
+val_SamplePCA_X,val_SamplePCA_Y = X[val[int(test*len(val)):int(train*len(val))]],ext.Y[val[int(test*len(val)):int(train*len(val))]]
+val_SampleX,val_SampleY = ext.X[val[int(test*len(val)):int(train*len(val))]],ext.Y[val[int(test*len(val)):int(train*len(val))]]
+
+print("train set size: {}".format(len(train_SampleY)))
+print("test set size: {}".format(len(test_SampleY)))
+print("validation set size: {}".format(len(val_SampleY)))
 
 pca_score = []
 imp_score = []
@@ -90,6 +101,7 @@ chi2_score = []
 sum_score = []
 n_feature = []
 
+best = {}
 
 for i in sorted(list(set(list(range(0,1650,100))+list(range(1600,1650,10))))):
     print("\nScores with best {} features/components:\n".format(1650-i))
@@ -98,26 +110,47 @@ for i in sorted(list(set(list(range(0,1650,100))+list(range(1600,1650,10))))):
     print("Scores before PCA:")
     etc.fit(train_SampleX[:,imp[:int(1650-i)]],train_SampleY)
     score = etc.score(test_SampleX[:,imp[:int(1650-i)]],test_SampleY)
+    if best.get("imp",(0,0))[1] < score:
+      best["imp"] = (int(1650-i),score)
     imp_score.append(score)
     print("\t -imp :\t{}".format(score))
     etc.fit(train_SampleX[:,f_c[:int(1650-i)]],train_SampleY)
     score = etc.score(test_SampleX[:,f_c[:int(1650-i)]],test_SampleY)
+    if best.get("f_c",(0,0))[1] < score:
+      best["f_c"] = (int(1650-i),score)
     f_c_score.append(score)
     print("\t -f_classif :\t{}".format(score))
     etc.fit(train_SampleX[:,chi2[:int(1650-i)]],train_SampleY)
     score = etc.score(test_SampleX[:,chi2[:int(1650-i)]],test_SampleY)
+    if best.get("chi2",(0,0))[1] < score:
+      best["chi2"] = (int(1650-i),score)
     chi2_score.append(score)
     print("\t -chi2 :\t{}".format(score))
     etc.fit(train_SampleX[:,sumv[:int(1650-i)]],train_SampleY)
     score = etc.score(test_SampleX[:,sumv[:int(1650-i)]],test_SampleY)
+    if best.get("sum",(0,0))[1] < score:
+      best["sum"] = (int(1650-i),score)
     sum_score.append(score)
     print("\t -Sum :\t{}".format(score))
 
     etc.fit(train_SamplePCA_X[:,:int(1650-i)],train_SamplePCA_Y)
     score = etc.score(test_SamplePCA_X[:,:int(1650-i)],test_SamplePCA_Y)
+    if best.get("pca",(0,0))[1] < score:
+      best["pca"] = (int(1650-i),score)
     pca_score.append(score)
     print("Score after PCA: {}".format(score))
 
+print("Best: {}".format(best))
+for f in best:
+    score_test,i = best[f]
+    if f == "pca":
+        etc.fit(train_SamplePCA_X[:,:int(1650-i)],train_SamplePCA_Y)
+        score = etc.score(val_SamplePCA_X[:,:int(1650-i)],val_SamplePCA_Y)
+        print("Score PCA:\t{}".format(score))
+    else:
+        etc.fit(train_SampleX[:,sumv[:int(1650-i)]],train_SampleY)
+        score = etc.score(test_SampleX[:,sumv[:int(1650-i)]],test_SampleY)
+        print("Score {}:\t{}".format(f,score))
 
 plt.plot(n_feature,pca_score,label="pca")
 plt.plot(n_feature,chi2_score,label="chi2")
@@ -138,8 +171,8 @@ tsne_score = {}
 for name,n in [("tsne2",2),("tsne3",3)]:
     print("fit_transform TSNE {}".format(n))
     tsne = TSNE(n,n_iter=1000).fit_transform(X[:,:i+1])
-    test_SampleTSNE_X,test_SampleTSNE_Y = tsne[val[:int(0.2*len(val))]],ext.Y[val[:int(0.2*len(val))]]
-    train_SampleTSNE_X,train_SampleTSNE_Y = tsne[val[int(0.2*len(val)):]],ext.Y[val[int(0.2*len(val)):]]
+    test_SampleTSNE_X,test_SampleTSNE_Y = tsne[val[:int(test*len(val))]],ext.Y[val[:int(test*len(val))]]
+    train_SampleTSNE_X,train_SampleTSNE_Y = tsne[val[int(train*len(val)):]],ext.Y[val[int(train*len(val)):]]
     etc.fit(train_SampleTSNE_X,train_SampleTSNE_Y)
     score = etc.score(test_SampleTSNE_X,test_SampleTSNE_Y)
     tsne_score[name] = score
