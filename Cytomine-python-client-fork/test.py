a434ec0e5023a9eb5801b219cd7a438e2360d566
20123161 <maxime.tasset@student.ulg.ac.be>
2018-07-27 17:52:32 +0200

    update

--- Cytomine-python-client-fork/test.py
+++ Cytomine-python-client-fork/test.py
@@ -38,8 +38,8 @@ cytomine_public_key="XXX"
 cytomine_private_key="XXX"
 id_project=0
 
-n_jobs = 16
-n_estimators = 1000
+n_jobs = 3
+n_estimators = 100
 test = .2
 validation = .1
 train = test + validation
@@ -62,11 +62,15 @@ pca = PCA().fit(ext.X)
 print("transform")
 X = pca.transform(ext.X)
 
+print("fit_transform pca with whiten")
+pca_w = PCA(whiten=True).fit(ext.X)
+print("transform")
+X_w = pca_w.transform(ext.X)
+
 indexes = list(range(X.shape[0]))
 shuffle(indexes)
 
-print("ExtraTreesClassifier with {} estimators".format(n_estimators))
-etc = ETC(n_jobs=n_jobs,n_estimators=n_estimators)
+etc = None
 
 
 def test_comparaisonFeatureImportance():
@@ -106,21 +110,31 @@ def test_comparaisonFeatureImportance():
     sumv = [i for val,i in sorted([(val,i) for i,val in enumerate(sum_val)])]
     del imp_val,f_c_val,chi2_val
 
-    #Tes, train and validation sets
-    test_SamplePCA_X,test_SamplePCA_Y = X[indexes[:int(test*len(indexes))]],ext.Y[indexes[:int(test*len(indexes))]]
-    test_SampleX,test_SampleY = ext.X[indexes[:int(test*len(indexes))]],ext.Y[indexes[:int(test*len(indexes))]]
+    #Test, train and validation sets
+    test_SamplePCA_X = X[indexes[:int(test*len(indexes))]]
+    test_SamplePCA_w_X = X_w[indexes[:int(test*len(indexes))]]
+    test_SampleX = ext.X[indexes[:int(test*len(indexes))]]
 
-    train_SamplePCA_X,train_SamplePCA_Y = X[indexes[int(train*len(indexes)):]],ext.Y[indexes[int(train*len(indexes)):]]
-    train_SampleX,train_SampleY = ext.X[indexes[int(train*len(indexes)):]],ext.Y[indexes[int(train*len(indexes)):]]
+    test_SampleY = ext.Y[indexes[:int(test*len(indexes))]]
 
-    val_SamplePCA_X,val_SamplePCA_Y = X[indexes[int(test*len(indexes)):int(train*len(indexes))]],ext.Y[indexes[int(test*len(indexes)):int(train*len(indexes))]]
-    val_SampleX,val_SampleY = ext.X[indexes[int(test*len(indexes)):int(train*len(indexes))]],ext.Y[indexes[int(test*len(indexes)):int(train*len(indexes))]]
+    train_SamplePCA_X = X[indexes[int(train*len(indexes)):]]
+    train_SamplePCA_w_X = X_w[indexes[int(train*len(indexes)):]]
+    train_SampleX = ext.X[indexes[int(train*len(indexes)):]]
+
+    train_SampleY = ext.Y[indexes[int(train*len(indexes)):]]
+
+    val_SamplePCA_X = X[indexes[int(test*len(indexes)):int(train*len(indexes))]]
+    val_SamplePCA_w_X = X_w[indexes[int(test*len(indexes)):int(train*len(indexes))]]
+    val_SampleX = ext.X[indexes[int(test*len(indexes)):int(train*len(indexes))]]
+
+    val_SampleY = ext.Y[indexes[int(test*len(indexes)):int(train*len(indexes))]]
 
     print("train set size: {}".format(len(train_SampleY)))
     print("test set size: {}".format(len(test_SampleY)))
     print("validation set size: {}".format(len(val_SampleY)))
 
     pca_score = []
+    pca_w_score = []
     imp_score = []
     f_c_score = []
     chi2_score = []
@@ -159,26 +173,38 @@ def test_comparaisonFeatureImportance():
         sum_score.append(score)
         print("\t -Sum :\t{}".format(score))
 
-        etc.fit(train_SamplePCA_X[:,:int(1650-i)],train_SamplePCA_Y)
-        score = etc.score(test_SamplePCA_X[:,:int(1650-i)],test_SamplePCA_Y)
+        etc.fit(train_SamplePCA_X[:,:int(1650-i)],train_SampleY)
+        score = etc.score(test_SamplePCA_X[:,:int(1650-i)],test_SampleY)
         if best.get("pca",(0,0))[1] < score:
           best["pca"] = (int(1650-i),score,None)
         pca_score.append(score)
         print("Score after PCA: {}".format(score))
 
+        etc.fit(train_SamplePCA_w_X[:,:int(1650-i)],train_SampleY)
+        score = etc.score(test_SamplePCA_w_X[:,:int(1650-i)],test_SampleY)
+        if best.get("pca_w",(0,0))[1] < score:
+          best["pca_w"] = (int(1650-i),score,None)
+        pca_w_score.append(score)
+        print("Score after PCA with whiten: {}".format(score))
+
     print("Bests tested on validation set:")
     for f in best:
         n_feature_k,score_test,f_imp = best[f]
         if f == "pca":
-            etc.fit(train_SamplePCA_X[:,:int(n_feature_k)],train_SamplePCA_Y)
-            score = etc.score(val_SamplePCA_X[:,:int(n_feature_k)],val_SamplePCA_Y)
+            etc.fit(train_SamplePCA_X[:,:int(n_feature_k)],train_SampleY)
+            score = etc.score(val_SamplePCA_X[:,:int(n_feature_k)],val_SampleY)
             print("Score PCA (with {} cpts):\t{} (test:\t{})".format(n_feature_k,score,score_test))
+        elif f == "pca_w":
+            etc.fit(train_SamplePCA_w_X[:,:int(n_feature_k)],train_SampleY)
+            score = etc.score(val_SamplePCA_w_X[:,:int(n_feature_k)],val_SampleY)
+            print("Score PCA with Whiten (with {} cpts):\t{} (test:\t{})".format(n_feature_k,score,score_test))
         else:
             etc.fit(train_SampleX[:,f_imp[:int(n_feature_k)]],train_SampleY)
             score = etc.score(val_SampleX[:,f_imp[:int(n_feature_k)]],val_SampleY)
             print("Score {} (with {} features):\t{} (test:\t{})".format(f,n_feature_k,score,score_test))
 
     plt.plot(n_feature,pca_score,label="pca")
+    plt.plot(n_feature,pca_w_score,label="pca whithen")
 
     plt.plot(n_feature,chi2_score,label="chi2")
     plt.plot(n_feature,imp_score,label="imp")
@@ -190,7 +216,7 @@ def test_comparaisonFeatureImportance():
     plt.savefig("comparaison_feature_imp_{}.png".format(n_estimators))
     plt.close()
 
-    return pca_score, imp_score, f_c_score, chi2_score, sum_score, n_feature
+    return pca_score,pca_w_score, imp_score, f_c_score, chi2_score, sum_score, n_feature
 
 def test_DimensionReduction():
   print("================================================")
@@ -215,66 +241,115 @@ def test_DimensionReduction():
   return counts
 
 def test_depth():
-  print("================================================")
-  print("Test: Depth Importance (etc max_depth)")
-  print("================================================")
-  max_depth = etc.max_depth
+    print("================================================")
+    print("Test: Depth Importance (etc max_depth)")
+    print("================================================")
+    max_depth = etc.max_depth
 
-  train_SampleX,train_SampleY = ext.X[indexes[int(train*len(indexes)):]],ext.Y[indexes[int(train*len(indexes)):]]
-  test_SampleX,test_SampleY = ext.X[indexes[:int(test*len(indexes))]],ext.Y[indexes[:int(test*len(indexes))]]
-  val_SampleX,val_SampleY = ext.X[indexes[int(test*len(indexes)):int(train*len(indexes))]],ext.Y[indexes[int(test*len(indexes)):int(train*len(indexes))]]
-  depth = 100
-  scores = []
-  best = (0,0,0)
-  for i in range(depth):
-      etc.max_depth = i + 1
-      etc.fit(train_SampleX,train_SampleY)
-      score = etc.score(test_SampleX,test_SampleY)
-      scores.append(score)
-      if score >= best[1]:
-        best = (i+1,score,etc.score(val_SampleX,val_SampleY))
-  plt.plot(range(1,depth+1),scores)
-  plt.ylabel("Score")
-  plt.xlabel("Max Depth")
-  plt.savefig("score_max_depth.png")
-  plt.close()
-  print("Best score with a max depth of {} (test set {}):\t{} on the validation set".format(best[0],best[1],best[2]))
-  etc.max_depth = max_depth
+    #Test, train and validation sets
+    test_SamplePCA_X = X[indexes[:int(test*len(indexes))]]
+    test_SamplePCA_w_X = X_w[indexes[:int(test*len(indexes))]]
+    test_SampleX = ext.X[indexes[:int(test*len(indexes))]]
+
+    test_SampleY = ext.Y[indexes[:int(test*len(indexes))]]
+
+    train_SamplePCA_X = X[indexes[int(train*len(indexes)):]]
+    train_SamplePCA_w_X = X_w[indexes[int(train*len(indexes)):]]
+    train_SampleX = ext.X[indexes[int(train*len(indexes)):]]
+
+    train_SampleY = ext.Y[indexes[int(train*len(indexes)):]]
+
+    val_SamplePCA_X = X[indexes[int(test*len(indexes)):int(train*len(indexes))]]
+    val_SamplePCA_w_X = X_w[indexes[int(test*len(indexes)):int(train*len(indexes))]]
+    val_SampleX = ext.X[indexes[int(test*len(indexes)):int(train*len(indexes))]]
+
+    val_SampleY = ext.Y[indexes[int(test*len(indexes)):int(train*len(indexes))]]
+
+    depth = 100
+
+    scores = []
+    scores_pca = []
+    scores_pca_w = []
+
+    best = (0,0,0)
+    best_pca = (0,0,0)
+    best_pca_w = (0,0,0)
+
+    for i in range(depth):
+        etc.max_depth = i + 1
+
+        etc.fit(train_SampleX,train_SampleY)
+        score = etc.score(test_SampleX,test_SampleY)
+        scores.append(score)
+        if score >= best[1]:
+          best = (i+1,score,etc.score(val_SampleX,val_SampleY))
+
+        etc.fit(train_SamplePCA_X,train_SampleY)
+        score = etc.score(test_SamplePCA_X,test_SampleY)
+        scores_pca.append(score)
+        if score >= best_pca[1]:
+          best_pca = (i+1,score,etc.score(val_SamplePCA_X,val_SampleY))
+
+        etc.fit(train_SamplePCA_w_X,train_SampleY)
+        score = etc.score(test_SamplePCA_w_X,test_SampleY)
+        scores_pca_w.append(score)
+        if score >= best_pca_w[1]:
+          best_pca_w = (i+1,score,etc.score(val_SamplePCA_w_X,val_SampleY))
+
+    plt.plot(range(1,depth+1),scores,"raw data")
+    plt.plot(range(1,depth+1),scores_pca,"PCA")
+    plt.plot(range(1,depth+1),scores_pca_w,"PCA whiten")
+    plt.ylabel("Score")
+    plt.xlabel("Max Depth")
+    plt.legend()
+    plt.savefig("score_max_depth.png")
+    plt.close()
+    print("Raw: Best score with a max depth of {} (test set {}):\t{} on the validation set".format(*best))
+    print("PCA: Best score with a max depth of {} (test set {}):\t{} on the validation set".format(*best_pca))
+    print("PCA with whiten: Best score with a max depth of {} (test set {}):\t{} on the validation set".format(*best_pca_w))
+
+    etc.max_depth = max_depth
 
 
 def test_Spaciality():
-  print("================================================")
-  print("Test: Spaciality Importance (tile size)")
-  print("================================================")
-  best = (0,0,0)
-  for i in range(1,11):
-    X,y = ext.rois2data(None,(i,i))
-    indexes = list(range(X.shape[0]))
-    shuffle(indexes)
-    train_SampleX,train_SampleY = X[indexes[int(train*len(indexes)):]],y[indexes[int(train*len(indexes)):]]
-    test_SampleX,test_SampleY = X[indexes[:int(test*len(indexes))]],y[indexes[:int(test*len(indexes))]]
-    val_SampleX,val_SampleY = X[indexes[int(test*len(indexes)):int(train*len(indexes))]],y[indexes[int(test*len(indexes)):int(train*len(indexes))]]
-    del X,y
+    print("================================================")
+    print("Test: Spaciality Importance (tile size)")
+    print("================================================")
+    best = (0,0,0)
+    for i in range(1,11):
+        X,y = ext.rois2data(None,(i,i))
+        indexes = list(range(X.shape[0]))
+        shuffle(indexes)
+        train_SampleX,train_SampleY = X[indexes[int(train*len(indexes)):]],y[indexes[int(train*len(indexes)):]]
+        test_SampleX,test_SampleY = X[indexes[:int(test*len(indexes))]],y[indexes[:int(test*len(indexes))]]
+        val_SampleX,val_SampleY = X[indexes[int(test*len(indexes)):int(train*len(indexes))]],y[indexes[int(test*len(indexes)):int(train*len(indexes))]]
+        del X,y
 
-    print("train set size: {}".format(len(train_SampleY)))
-    print("test set size: {}".format(len(test_SampleY)))
-    print("validation set size: {}".format(len(val_SampleY)))
+        print("train set size: {}".format(len(train_SampleY)))
+        print("test set size: {}".format(len(test_SampleY)))
+        print("validation set size: {}".format(len(val_SampleY)))
 
-    etc.fit(train_SampleX,train_SampleY)
-    del train_SampleX,train_SampleY
+        etc.fit(train_SampleX,train_SampleY)
+        del train_SampleX,train_SampleY
 
-    score = etc.score(test_SampleX,test_SampleY)
-    del test_SampleX,test_SampleY
-    print("Score with a slice size of {}:\t{}".format(i,score))
+        score = etc.score(test_SampleX,test_SampleY)
+        del test_SampleX,test_SampleY
+        print("Score with a slice size of {}:\t{}".format(i,score))
 
-    if score >= best[1]:
-        best = (i,score,etc.score(val_SampleX,val_SampleY))
-    del val_SampleX,val_SampleY
+        if score >= best[1]:
+            best = (i,score,etc.score(val_SampleX,val_SampleY))
+        del val_SampleX,val_SampleY
 
-  print("Best score with a slice size of {} (test set {}):\t{} on the validation set".format(best[0],best[1],best[2]))
+    print("Best score with a slice size of {} (test set {}):\t{} on the validation set".format(best[0],best[1],best[2]))
+
 
 if __name__ == '__main__':
-  test_comparaisonFeatureImportance()
-  counts = test_DimensionReduction()
-  test_depth()
-  test_Spaciality()
+
+    for n_estimators in [100,1000]:
+        print("ExtraTreesClassifier with {} estimators".format(n_estimators))
+        etc = ETC(n_jobs=n_jobs,n_estimators=n_estimators)
+        test_comparaisonFeatureImportance()
+        counts = test_DimensionReduction()
+        test_depth()
+        test_Spaciality()
+
