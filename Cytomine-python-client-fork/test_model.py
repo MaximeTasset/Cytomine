054cd7404b30fcccbf41eda498c990967efe7d99
20123161 <maxime.tasset@student.ulg.ac.be>
2018-07-30 17:28:51 +0200

    update

--- Cytomine-python-client-fork/test_model.py
+++ Cytomine-python-client-fork/test_model.py
@@ -22,7 +22,7 @@ id_project = 56924820
 def estimator():
     return ExtraTreesClassifier(n_estimators=1000,n_jobs=n_jobs)
 
-with Cytomine(host="demo.cytomine.be", public_key="f1f8cacc-b71a-4bc2-a6cd-e6bb40fd19b5", private_key="9e94aa70-4e7c-4152-8067-0feeb58d42eb",
+with Cytomine(host="demo.cytomine.be", public_key="XXX", private_key="XXX",
                   verbose=logging.WARNING) as cytomine:
     ig = ImageGroupCollection({"project":56924820}).fetch()[0]
     ext = Extractor(filename)
@@ -31,7 +31,7 @@ with Cytomine(host="demo.cytomine.be", public_key="f1f8cacc-b71a-4bc2-a6cd-e6bb4
     sm = SpectralModel(base_estimator=estimator,n_estimators=1,step=1,slice_size=(5,5),n_jobs=1)
     sm.fit(ext.rois,use=1)
 
-    reader = CytomineSpectralReader(ig,Bounds(5531,2353,512,512),tile_size=Bounds(0,0,512,512),n_jobs=8)
+    reader = CytomineSpectralReader(ig.id,Bounds(5531,2353,512,512),tile_size=Bounds(0,0,512,512),num_thread=8)
     reader.read()
     muli_image = reader.getResult(True,False)
     with open("flutiste_data.im",'wb') as fp:
